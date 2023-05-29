e3d7c40af26919756b0992bc4d4a21a77394b2fc
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-05-29 09:18:50 +0200

    Create: pages,  overview page contents,  Detail pages with links,  add volume covers

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Display link to the volume overview.</h1>
+      <Link href="/volumes">Volume Overview</Link>
     </div>
   );
 }
---
+++ pages/volumes/index.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { introduction, volumes } from "../../lib/data";
+
+export default function Volumes() {
+  return (
+    <>
+      <h1>Lord of the Rings</h1>
+      <div>
+        <p>{introduction}</p>
+      </div>
+
+      <div>
+        <h2>All Volumes</h2>
+        <ul>
+          {volumes.map((volume) => {
+            return (
+              <li key={volume.slug}>
+                <Link href={`volumes/${volume.slug}`}> {volume.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <ul></ul>
+    </>
+  );
+}
